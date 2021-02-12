d21b78b1e555a38b09426578e84d7f526fb10438
Miguel Mercado <miguel.mercadosalgado@gmail.com>
2021-02-12 18:55:50 +0100

    Create JS get all pokemons

--- public/js/getAll.js
+++ public/js/getAll.js
@@ -1,1 +1,21 @@
-console.log("running");
+const urlApi = "http://localhost:1212/api/pokemons";
+const list = document.querySelector(".poke-list");
+
+const getAllPokemons = () => {
+  fetch(urlApi)
+    .then((response) => response.json())
+    .then((data) => {
+      //console.log(data);
+      let pokeList = data.pokemons
+        .map(
+          (pokemon) =>
+            `<li class="text-center list-group-item">${pokemon.name}</li>`
+        )
+        .join("");
+      list.innerHTML = pokeList;
+    })
+    .catch((error) => console.error(error))
+    .finally(() => console.log("fetch done"));
+};
+
+getAllPokemons();
